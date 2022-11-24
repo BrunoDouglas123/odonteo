498e10bc1ffcfc6be39b46dde7ab878d75eb9b9f
Bruno Douglas <douglasbruno300@gmail.com>
2022-11-24 20:56:04 -0300

    odeio front

---
+++ src/tests/App.test.js
@@ -0,0 +1,40 @@
+import { render, screen } from "@testing-library/react";
+import App from "../App.js";
+
+describe("Test sanity of routes", () => {
+  beforeEach(() => {
+    const currentState = window.history.state;
+
+    window.history.replaceState(currentState, "", "/");
+  });
+
+  it("Should render login page", async () => {
+    window.history.pushState({}, "Login page", "/login");
+
+    render(<App />);
+
+    const loginPage = screen.getByTestId("login-test-id");
+
+    expect(loginPage).toBeInTheDocument();
+  });
+
+  // it('Should render main page', async () => {
+  //     window.history.pushState({}, 'Main page', '/')
+
+  //     render(<App/>)
+
+  //     const mainPage = screen.getByTestId('main-test-id');
+
+  //     expect(mainPage).toBeInTheDocument();
+  // });
+
+  // it('Should render statement page', async () => {
+  //     window.history.pushState({}, 'Statement page', '/statement')
+
+  //     render(<App/>)
+
+  //     const statementPage = screen.getByTestId('statement-test-id');
+
+  //     expect(mainPage).toBeInTheDocument();
+  // });
+});
--- src/utils/parseInstallmentDetails.js
+++ src/utils/parseInstallmentDetails.js
@@ -1,25 +1,25 @@
 export default function parseInstallmentDetails(
-    paymentAmount,
-    numberOfInstallments,
-    billingDay,
-    firstInstallmentDate
-  ) {
-  const installmentValue = Math
-    .floor((Number(paymentAmount) / Number(numberOfInstallments)) * 100) / 100;
-  
+  paymentAmount,
+  numberOfInstallments,
+  billingDay,
+  firstInstallmentDate,
+) {
+  const installmentValue =
+    Math.floor((Number(paymentAmount) / Number(numberOfInstallments)) * 100) /
+    100;
+
   // ^ Código consultado na seguinte fonte: https://tutorial.eyehunts.com/js/javascript-format-number-2-decimals-without-rounding-example-code/
-  
+
   const installmentDetails = {
     dates: [firstInstallmentDate],
-    installmentValue
+    installmentValue,
   };
 
-
-  let [year, month] = firstInstallmentDate.split('-');
+  let [year, month] = firstInstallmentDate.split("-");
   year = Number(year);
-  
+
   const shortMonths = [4, 6, 9, 11];
-  
+
   for (let i = 1; i < Number(numberOfInstallments); i += 1) {
     let day = Number(billingDay);
     month = Number(month) + 1;
@@ -29,14 +29,13 @@ export default function parseInstallmentDetails(
       month = 1;
     }
 
-    if (day === 31 && shortMonths.some((element) => element === month)) {
+    if (day === 31 && shortMonths.some) {
       day = 30;
     }
 
     if (day >= 29 && month === 2) {
       day = 28;
     }
-
 
     if (month < 10) {
       month = `0${month}`;
